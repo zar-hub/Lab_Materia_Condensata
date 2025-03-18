9eb2859abfa4656fc28d55d0c7823a467690dad2
zar <64859915+zar-hub@users.noreply.github.com>
2025-03-18 16:17:44 +0100

    Improve responce function, work on na fitting

--- modules/utils.py
+++ modules/utils.py
@@ -4,6 +4,8 @@ import pandas as pd
 from pprint import pprint
 import sys
 from scipy.special import wofz
+import matplotlib
+from typing import Tuple
 
 _DEBUG = False
 
@@ -158,6 +160,77 @@ def check_paths(in_paths, out_paths):
         if not os.path.exists(abs_path):
             print(f'Creating path: [{abs_path}]')
             os.makedirs(abs_path)
+
+def save_fig(
+        fig: matplotlib.figure.Figure, 
+        fig_name: str, 
+        fig_dir: str, 
+        fig_fmt: str,
+        fig_size: Tuple[float, float] = [6.4, 4], 
+        save: bool = True, 
+        dpi: int = 300,
+        transparent_png = True,
+    ):
+    """This procedure stores the generated matplotlib figure to the specified 
+    directory with the specified name and format.
+
+    Parameters
+    ----------
+    fig : [type]
+        Matplotlib figure instance
+    fig_name : str
+        File name where the figure is saved
+    fig_dir : str
+        Path to the directory where the figure is saved
+    fig_fmt : str
+        Format of the figure, the format should be supported by matplotlib 
+        (additional logic only for pdf and png formats)
+    fig_size : Tuple[float, float]
+        Size of the figure in inches, by default [6.4, 4] 
+    save : bool, optional
+        If the figure should be saved, by default True. Set it to False if you 
+        do not want to override already produced figures.
+    dpi : int, optional
+        Dots per inch - the density for rasterized format (png), by default 300
+    transparent_png : bool, optional
+        If the background should be transparent for png, by default True
+    """
+    if not save:
+        return
+    
+    fig.set_size_inches(fig_size, forward=False)
+    fig_fmt = fig_fmt.lower()
+    fig_dir = os.path.join(fig_dir, fig_fmt)
+    if not os.path.exists(fig_dir):
+        os.makedirs(fig_dir)
+    pth = os.path.join(
+        fig_dir,
+        '{}.{}'.format(fig_name, fig_fmt.lower())
+    )
+    if fig_fmt == 'pdf':
+        metadata={
+            'Creator' : '',
+            'Producer': '',
+            'CreationDate': None
+        }
+        fig.savefig(pth, bbox_inches='tight', metadata=metadata)
+    elif fig_fmt == 'png':
+        alpha = 0 if transparent_png else 1
+        axes = fig.get_axes()
+        fig.patch.set_alpha(alpha)
+        for ax in axes:
+            ax.patch.set_alpha(alpha)
+        fig.savefig(
+            pth, 
+            bbox_inches='tight',
+            dpi=dpi,
+        )
+    else:
+        try:
+            fig.savefig(pth, bbox_inches='tight')
+        except Exception as e:
+            print("Cannot save figure: {}".format(e)) 
+
             
 # IGOR VOIGT FUNCTION WRAPPING
 # from CHATGPT but checked for correctness
